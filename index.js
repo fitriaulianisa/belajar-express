0d1946dd8376bd2a6fb734333e6499925bc79e40
fitriaulianisa <aulianisafitri434@gmail.com>
2024-09-24 11:18:38 +0700

    .

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000 // port
 // route /
 app.get("/", (req, res) => {
     // res.send("Hello");
-    res.sendFile(__dirname + "/.html")
+    res.sendFile(__dirname + "/index.html")
 });
 
 // route /about
