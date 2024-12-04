17644b45d7d3234b9e40c9faec56cef6be809289
fitriaulianisa <aulianisafitri434@gmail.com>
2024-12-04 22:59:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const expressLayout = require("express-ejs-layouts");
-const cors = require("cors"); // import cors
+// const cors = require("cors"); // import cors
 const connectDB = require("./app_api/models/db");
 
 var indexRouter = require('./app_server/routes/index');
